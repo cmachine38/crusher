e4a1f667d2f5e34ec3ce8d39868729aba4b5bdf5
cmachne38 <cmachne38@gmail.com>
2022-03-03 15:59:43 +0530

    updated

--- src/main/java/com/construction/crusher/controller/GreetingController.java
+++ src/main/java/com/construction/crusher/controller/GreetingController.java
@@ -1,6 +1,10 @@
 package com.construction.crusher.controller;
 
+import com.construction.crusher.model.Employee;
+import com.construction.crusher.repo.EmployeeRepository;
+import com.construction.crusher.service.EmployeeService;
 import com.construction.crusher.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -14,6 +18,10 @@ public class GreetingController {
 	//private UserService2 userService;
 
 	private UserService userService;
+
+	@Autowired
+	private EmployeeService employeeService;
+
 
 //	public GreetingController(UserService2 userService) {
 //		this.userService = userService;
@@ -50,6 +58,47 @@ public class GreetingController {
 	@ResponseBody
 	public String currentUserName(Authentication authentication) {
 		return authentication.getName();
+	}
+
+
+
+	@GetMapping("/employees")
+	public String all(Model model) {
+		System.out.println("***************************inside controller");
+		model.addAttribute("employees", employeeService.findAll());
+		return "employee_master";
+	}
+
+
+	@GetMapping("/employee_create")
+	public String create(Model model) {
+		model.addAttribute("newEmployee",new Employee());
+		return "employee_create";
+	}
+
+	@PostMapping("/employee_create_add")
+	public String addEmployee(@ModelAttribute Employee newEmployee, Model model) {
+		employeeService.create(newEmployee);
+
+		model.addAttribute("employees", employeeService.findAll());
+		return "employee_master";
+	}
+
+	@GetMapping("/employee_delete")
+	public String deleteEmployee(@RequestParam(value = "employeeAadhar", required = true) String employeeAadhar, Model model) {
+		Employee employee = employeeService.find(employeeAadhar);
+		employeeService.delete(employee);
+
+		model.addAttribute("employees", employeeService.findAll());
+		return "employee_master";
+	}
+
+	@GetMapping("/employee_edit")
+	public String editEmployee(@RequestParam(value = "employeeAadhar", required = true) String employeeAadhar, Model model) {
+		Employee employee = employeeService.find(employeeAadhar);
+
+		model.addAttribute("newEmployee", employee);
+		return "employee_create";
 	}
 
 }
